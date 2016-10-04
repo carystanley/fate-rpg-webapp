7c40d524aa7f2044823b139b2f76a74e427d8fd5
Cary Stanley <cary@yahoo-inc.com>
2016-10-04 16:51:01 -0700

    Add Superlatives

--- src/components/Roll.js
+++ src/components/Roll.js
@@ -1,12 +1,29 @@
 import React, { PropTypes } from 'react'
 
+const ladder = {
+  '-2': 'Terrible',
+  '-1': 'Poor',
+  '0': 'Mediocre',
+  '1': 'Average',
+  '2': 'Fair',
+  '3': 'Good',
+  '4': 'Great',
+  '5': 'Superb',
+  '6': 'Fantastic',
+  '7': 'Epic',
+  '8': 'Legendary'
+};
+
+const superlative = (value) => ladder['' + Math.min(Math.max(value, -2), 8)];
+const format = (value) => value > 0 ? '+'+value : ''+value;
+
 const Roll = React.createClass({
   render: function() {
     let {values, total} = this.props;
     return (
       <li ref={(ref) => this._li = ref}>
         <div className="roll">{values.map(value => <div className={'dice value' + value}><div className="vert" /><div className="horz" /></div>)}</div>
-        <div>Total: {total}</div>
+        <div className="roll-total">{format(total)} {superlative(total)}</div>
       </li>
     );
   },
