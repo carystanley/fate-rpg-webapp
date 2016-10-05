960f283ec75fe86ce8b5ca683733d8aec9d144b1
Cary Stanley <cary@yahoo-inc.com>
2016-10-05 09:36:44 -0700

    Add CSShake

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../css/main.css';
+import '../css/csshake-hard.css';
 
 import AddRoll from '../containers/AddRoll'
 import RollHistory from '../containers/RollHistory'
