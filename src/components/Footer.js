009e229af83974a5400f4b254587bade1b3de337
Cary Stanley <cary@yahoo-inc.com>
2016-10-07 16:27:09 -0700

    Add Simple Navigation

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,13 @@
 import React from 'react'
+import cx from 'classnames'
 
-let Footer = ({ onRoll, navigate }) => {
+let Footer = ({ onRoll, navigate, currentView }) => {
   return (
     <div id="footer" className="bgc-footer">
-      <button className="Btn" onClick={onRoll}>Roll</button>
-      <button className="Btn" onClick={() => { navigate('roll') }}>Current</button>
-      <button className="Btn" onClick={() => { navigate('history') }}>History</button>
+      <ul className="navigation">
+        <li className={cx({selected: currentView === 'roll'})} onClick={() => { navigate('roll') }}>Current</li>
+        <li className={cx({selected: currentView === 'history'})} onClick={() => { navigate('history') }}>History</li>
+      </ul>
     </div>
   )
 }
